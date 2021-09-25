a6c1a48ef91734f7810e298e5d4ee0baa2e28572
Rostyslav-Yefremov <rostislavefremow@gmail.com>
2021-09-25 17:19:55 +0300

    Add task-1

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+// const listRef = document.querySelectorAll('.item');
+// console.log(`В списке ${listRef.length} категории`);
+// listRef.forEach((el =>
+//   console.log(`Категория:${el.firstElementChild.textContent}
+// Количество элементов:${el.lastElementChild.children.length}`)
+// ));
+
+const itemsRef = document.querySelectorAll('.item');
+console.log(`В списке ${itemsRef.length} категории`);
+itemsRef.forEach(el => {
+const h2 = el.querySelector('h2');
+const elItems = el.querySelectorAll('li');
+    console.log(`Категория: ${h2.textContent} 
+Количество элементов: ${elItems.length}`);
+});
